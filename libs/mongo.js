96d7fc8397d009c6299d681a97b271c365e44b42
AbhishekJamdade <jamdadeabhishek039@gmail.com>
2025-06-10 15:15:18 +0530

    some adjustments

--- libs/mongo.js
+++ libs/mongo.js
@@ -1,4 +1,4 @@
-import { mongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 if (!process.env.MONGO_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGO_URI" ');
