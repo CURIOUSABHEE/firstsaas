68840a83cfbd3a7433f92e1aa20f8f8217ee8cf5
AbhishekJamdade <jamdadeabhishek039@gmail.com>
2025-06-10 15:14:05 +0530

    Added vercel analytics and connect authjs to mongodb

--- auth.js
+++ auth.js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "./libs/mongo";
 
 const config = {
   providers: [],
+  adapter: MongoDBAdapter(clientPromise),
 };
 
 export const { handlers, signOut, signIn, auth } = NextAuth(config);
